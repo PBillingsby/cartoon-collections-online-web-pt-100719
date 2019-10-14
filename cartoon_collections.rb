01559016f7a6cacc5a64dd72381a4eadf334f395
Phil Billingsby <paulbillingsby@gmail.com>
2019-10-14 20:40:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,4 @@
-=begin def roll_call_dwarves(dwarves)# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   dwarves.each_with_index {|item, index| puts "#{index + 1}. #{item}"}
 end
 
@@ -15,23 +15,4 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find {|item| cheese_types.include?(item)}
-end
-
-=end
-
-def roll_call_dwarves(dwarves)
-  dwarves.each_with_index {|item, index| puts "#{index + 1}. #{item}"}
-end
-
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect {|name| name.capitalize + "!"}
-end
-
-def long_planeteer_calls(array)
-    array.any? {|n| n.length > 4}
-end
-
-def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find {|n| cheese_types.include?(n)}
 end
