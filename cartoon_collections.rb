44a64ad1e2dbd08594d2b6366d33cf240d692f98
Phil Billingsby <paulbillingsby@gmail.com>
2019-10-14 20:33:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,3 +22,7 @@ end
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index {|item, index| puts "#{index + 1}. #{item}"}
 end
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect {|name| name.capitalize + "!"}
+end
