28f69e9562eadfb1b0938f92efda8e55d3f876f4
Phil Billingsby <paulbillingsby@gmail.com>
2019-10-14 20:39:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,3 +30,7 @@ end
 def long_planeteer_calls(array)
     array.any? {|n| n.length > 4}
 end
+
+def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.any? {|n| n in cheese_types}
